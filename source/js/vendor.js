741c944f7268a6c4e029b7472d6a62d87247db0e
VasiliCheb <skimanvasia@gmail.com>
2023-09-28 20:39:04 +0300

    feat: загружает и подключает в проект gsap и плагин скролл-триггер

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,3 +1,7 @@
 // Swiper 8.4.7
 // import './vendor/swiper';
 import './vendor/focus-visible-polyfill';
+import {gsap} from './vendor/gsap/gsap.min';
+import {ScrollTrigger} from './vendor/gsap/scroll-trigger.min';
+
+gsap.registerPlugin(ScrollTrigger);
